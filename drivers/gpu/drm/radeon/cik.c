81ffb5cd7bba750eee81b100084c4d32a90275da
Oded Gabbay <oded.gabbay@amd.com>
2014-02-05 17:49:34 +0200

    drm/radeon: Add calls to suspend and resume of kfd driver
    The radeon driver can suspend and resume its device. For each device it
    suspends/resumes, it should inform the kfd about it, so the kfd could
    perform relevant actions per that device.
    
    This patch adds the calls to kfd's suspend and resume functions. The
    device is passed as an argument.
    
    Signed-off-by: Oded Gabbay <oded.gabbay@amd.com>

--- drivers/gpu/drm/radeon/cik.c
+++ drivers/gpu/drm/radeon/cik.c
@@ -138,6 +138,8 @@ static void cik_fini_pg(struct radeon_device *rdev);
 static void cik_fini_cg(struct radeon_device *rdev);
 static void cik_enable_gui_idle_interrupt(struct radeon_device *rdev,
 					  bool enable);
+extern void radeon_kfd_suspend(struct radeon_device *rdev);
+extern int radeon_kfd_resume(struct radeon_device *rdev);
 
 /* get temperature in millidegrees */
 int ci_get_temp(struct radeon_device *rdev)
@@ -8429,6 +8431,10 @@ static int cik_startup(struct radeon_device *rdev)
 	if (r)
 		return r;
 
+	r = radeon_kfd_resume(rdev);
+	if (r)
+		return r;
+
 	return 0;
 }
 
@@ -8477,6 +8483,7 @@ int cik_resume(struct radeon_device *rdev)
  */
 int cik_suspend(struct radeon_device *rdev)
 {
+	radeon_kfd_suspend(rdev);
 	radeon_pm_suspend(rdev);
 	dce6_audio_fini(rdev);
 	radeon_vm_manager_fini(rdev);
