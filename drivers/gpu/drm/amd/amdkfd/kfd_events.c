e9f45a7101163e15572bdbb16abd0969f17b49a1
Oded Gabbay <oded.gabbay@amd.com>
2014-06-01 18:34:24 +0300

    hsa/radeon: Prevent destroying an event with waiters
    This patch fixes a bug in KFD where you could destroy an event that has active
    waiters on it.
    
    Signed-off-by: Oded Gabbay <oded.gabbay@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_events.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_events.c
@@ -413,8 +413,15 @@ int kfd_event_destroy(struct kfd_process *p, uint32_t event_id)
 	mutex_lock(&p->event_mutex);
 
 	ev = lookup_event_by_id(p, event_id);
+
 	if (ev)
-		destroy_event(ev);
+		if (!list_empty(&ev->waiters)) {
+			pr_err("kfd: can't destroy event with waiters\n");
+			ret = -EBUSY;
+		}
+		else {
+			destroy_event(ev);
+		}
 	else
 		ret = -EINVAL;
 
