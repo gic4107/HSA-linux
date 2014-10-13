fecdccd18993e0562de552cc8c2fbe65563bbe0d
Oded Gabbay <oded.gabbay@amd.com>
2014-10-13 09:49:15 +0300

    amdkfd: Change driver version to 0.9.9-rc1
    Signed-off-by: Oded Gabbay <oded.gabbay@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_module.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_module.c
@@ -29,11 +29,11 @@
 #define KFD_DRIVER_AUTHOR	"AMD Inc. and others"
 
 #define KFD_DRIVER_DESC		"Standalone HSA driver for AMD's GPUs"
-#define KFD_DRIVER_DATE		"20140923"
+#define KFD_DRIVER_DATE		"20141013"
 #define KFD_DRIVER_MAJOR	0
 #define KFD_DRIVER_MINOR	9
-#define KFD_DRIVER_PATCHLEVEL	0
-#define KFD_DRIVER_RC_LEVEL	"-rc3"
+#define KFD_DRIVER_PATCHLEVEL	9
+#define KFD_DRIVER_RC_LEVEL	"-rc1"
 
 const struct kfd2kgd_calls *kfd2kgd;
 static const struct kgd2kfd_calls kgd2kfd = {
